50e18a2f541b378a7fcbf832db6f31c6811623c4
[qavi-nizamani] <[qaviniz11@gmail.com]>
2021-03-27 19:44:17 +0500

    Cookie cleared from client side and database

--- routes/logout.js
+++ routes/logout.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-router.get("/", auth, (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
+    req.user.tokens = req.user.tokens.filter((element) => {
+      return element.token !== req.token;
+    });
     res.clearCookie("jwt");
+    await req.user.save();
     res.status(200).render("login");
   } catch (error) {
     res.status(404).send(error);
